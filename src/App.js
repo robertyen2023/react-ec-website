dc7544aaacb221b91a31434ef9d9db029e7ae02a
robertyen2023 <robertyen2023@gmail.com>
2024-01-19 23:55:25 +0800

    Extract the category data objects; Map the category component from data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-const App = () => (
-  <div className="categories-container">
-    <div className="category-container">
+const App = () => {
+  const categoriesData = [
+    {
+      id: 1,
+      h2Title: 'Mens'
+    },
+    {
+      id: 2,
+      h2Title: 'Womens'
+    },
+    {
+      id: 3,
+      h2Title: 'Shoes'
+    }
+  ];
+  const categories = categoriesData.map(({ id = 0, h2Title = '' } = {} ) => (
+    <div 
+      className="category-container"
+      key={id}
+    >
       {/* <img/> */}
       <div className="category-body-container">
-        <h2>Mens</h2>
+        <h2>{h2Title}</h2>
         <p>Shop Now</p>
       </div>
     </div>
-    <div className="category-container">
-      {/* <img/> */}
-      <div className="category-body-container">
-        <h2>Womens</h2>
-        <p>Shop Now</p>
-      </div>
+  ));
+
+  return (
+    <div className="categories-container">
+      {categories}
     </div>
-    <div className="category-container">
-      {/* <img/> */}
-      <div className="category-body-container">
-        <h2>Shoes</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default App;
