7541ebe165e7577b85ae02b3ce9e1d078094d068
robertyen2023 <robertyen2023@gmail.com>
2024-01-31 05:15:13 +0800

    5.84. Setup the root route for rendering Home Component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import './App.scss';
+import { Routes, Route } from 'react-router-dom';
 
-import Directory from './components/Directory/Directory';
+import Home from './routes/Home/Home';
 
 const App = () => {
-  const categoriesData = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }  
-  ];
-  
-
   return (
-    <div className="app-container">
-      <Directory categoriesData={categoriesData} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
