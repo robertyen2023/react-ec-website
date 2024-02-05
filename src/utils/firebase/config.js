b9cd1d1a6e829d1ee86a9747f421ff7d6f6cffa8
robertyen2023 <robertyen2023@gmail.com>
2024-02-05 23:56:00 +0800

    Add firebase SDK / config

---
+++ src/utils/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIFudx30r4nHD_IiQm22a70cJ9p-WoNhY",
+  authDomain: "react-ec-website-db.firebaseapp.com",
+  projectId: "react-ec-website-db",
+  storageBucket: "react-ec-website-db.appspot.com",
+  messagingSenderId: "83736969615",
+  appId: "1:83736969615:web:360436380bd171c2540bfa"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
