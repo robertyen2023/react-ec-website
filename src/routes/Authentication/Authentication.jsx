662773250626e8056cf533790b7aa78396cd26f1
robertyen2023 <robertyen2023@gmail.com>
2024-03-03 18:56:22 +0800

    104.4. Adjust the styling of Authentication pg Component

--- src/routes/Authentication/Authentication.jsx
+++ src/routes/Authentication/Authentication.jsx
@@ -1,74 +1,78 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
 
-import {
-    auth,
-    signInWithGooglePopup,
-    signInWithGoogleRedirect,
-    createUserDocumentFromAuth 
-} from '../../utils/firebase/config';
+// import {
+//     auth,
+//     signInWithGooglePopup,
+//     signInWithGoogleRedirect,
+//     createUserDocumentFromAuth 
+// } from '../../utils/firebase/config';
 
+import SignInForm from '../../components/SignInForm/SignInForm';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
-import Button, { BUTTON_TYPE_CLASSES } from '../../components/Button/Button';
+// import Button, { BUTTON_TYPE_CLASSES } from '../../components/Button/Button';
+
+import './Authentication.scss';
 
 const Authentication = () => {
-    useEffect(() => {
-        // useEffect must NOT return anything besides a function
-        // for example, an async function is not valid
-        // .
-        // that's why we need to wrap an async function 
-        // inside of the callback of useEffect
-        // like below 
-        // (instead of make [the callback async like `useEffect(async () => { .. }, [])`])
-        const fetchRedirectResult = async () => {
-            // [the auth singleton] is like [an authentication memory bank]
-            // that tracks all of our authentication
-            // .
-            // even though our entire web app has unmounted from the browser
-            // (whenever the user [navigates away from our website])
-            // and redirect to other web app (i.e. google auth. pg)
-            // we can still apply [the getRedirectResult and auth singleton]
-            // to track the authentication memory
-            // to get [the authenticated user] info.
-            // in order for us to create the user document in the firestore
-            const response = await getRedirectResult(auth);
+    // useEffect(() => {
+    //     // useEffect must NOT return anything besides a function
+    //     // for example, an async function is not valid
+    //     // .
+    //     // that's why we need to wrap an async function 
+    //     // inside of the callback of useEffect
+    //     // like below 
+    //     // (instead of make [the callback async like `useEffect(async () => { .. }, [])`])
+    //     const fetchRedirectResult = async () => {
+    //         // [the auth singleton] is like [an authentication memory bank]
+    //         // that tracks all of our authentication
+    //         // .
+    //         // even though our entire web app has unmounted from the browser
+    //         // (whenever the user [navigates away from our website])
+    //         // and redirect to other web app (i.e. google auth. pg)
+    //         // we can still apply [the getRedirectResult and auth singleton]
+    //         // to track the authentication memory
+    //         // to get [the authenticated user] info.
+    //         // in order for us to create the user document in the firestore
+    //         const response = await getRedirectResult(auth);
 
-            // there's a chance we might get null response
-            // i.e. before applying [the Google Redirect User Flow]
-            if (response) {
-                const { user: userAuth } = response;
-                const userDocRef = await createUserDocumentFromAuth(userAuth);
-                console.log(userDocRef);
-            }
-        };
-        fetchRedirectResult();
-    }, []);
-    const logGoogleRedirectUser = async () => {
-        const { user: userAuth } = await signInWithGoogleRedirect();
-        // console.log(userAuth);
-    };
+    //         // there's a chance we might get null response
+    //         // i.e. before applying [the Google Redirect User Flow]
+    //         if (response) {
+    //             const { user: userAuth } = response;
+    //             const userDocRef = await createUserDocumentFromAuth(userAuth);
+    //             console.log(userDocRef);
+    //         }
+    //     };
+    //     fetchRedirectResult();
+    // }, []);
+    // const logGoogleRedirectUser = async () => {
+    //     const { user: userAuth } = await signInWithGoogleRedirect();
+    //     // console.log(userAuth);
+    // };
 
-    const logGoogleUser = async () => {
-        const { user: userAuth } = await signInWithGooglePopup();
-        const userDocRef= await createUserDocumentFromAuth(userAuth);
-        console.log(userDocRef);
-    };
+    // const logGoogleUser = async () => {
+    //     const { user: userAuth } = await signInWithGooglePopup();
+    //     const userDocRef= await createUserDocumentFromAuth(userAuth);
+    //     console.log(userDocRef);
+    // };
 
     return (
-        <div>
-            <div>
-                <Button
-                    buttonType={BUTTON_TYPE_CLASSES['GOOGLE-SIGN-IN']}
-                    type="submit"
-                    onClick={logGoogleUser}
-                >
-                    Sign In with Google Popup
-                </Button>
-                {/* <button onClick={logGoogleRedirectUser}>Sign In with Google Redirect</button> */}
-            </div>
-            <div>
-                <SignUpForm />
-            </div>
+        <div className='authentication-container'>
+            {/*             
+                <div>
+                    <Button
+                        buttonType={BUTTON_TYPE_CLASSES['GOOGLE-SIGN-IN']}
+                        type="submit"
+                        onClick={logGoogleUser}
+                    >
+                        Sign In with Google Popup
+                    </Button>
+                    <button onClick={logGoogleRedirectUser}>Sign In with Google Redirect</button>
+                </div> 
+            */}
+            <SignUpForm />
+            <SignInForm />
         </div>
     );
 };
