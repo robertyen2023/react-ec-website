44636eda11973604c86378e8c8c7374b88d02751
robertyen2023 <robertyen2023@gmail.com>
2024-03-17 00:03:28 +0800

    9.113. Add mock data json file (shop-data.json) and test it in the new (page) route (/shop)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Navagation from './routes/Navigation/Navigation';
 import Home from './routes/Home/Home';
 import Authentication from './routes/Authentication/Authentication';
+import Shop from './routes/Shop/Shop';
 
 // for test only
 // to test
 // 1. /, shop
 // 2. /home, shop // ***
 // 3. others // check 5.85 again when available
-const Shop = () => (<div>Shop Component</div>);
+// const Shop = () => (<div>Shop Component</div>);
 
 const App = () => {
   return (
