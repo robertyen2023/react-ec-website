8349cb2c7cfb7c5b31fe659365b0f6d76f0f2d7c
robertyen2023 <robertyen2023@gmail.com>
2024-03-17 01:37:32 +0800

    9.116.2. Add CartDropdown, the rough version (without data state), and use it in Navigation.

--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -6,6 +6,7 @@ import { UserContext } from '../../contexts/user-context';
 import { signOutUser } from '../../utils/firebase/config';
 import './Navigation.scss';
 import CartIcon from '../../components/CartIcon/CartIcon';
+import CartDropdown from '../../components/CartDropdown/CartDropdown';
 
 const Navigation = () => {
     const { 
@@ -50,6 +51,7 @@ const Navigation = () => {
                     { authOrSignOut }
                     <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </>    
