4799a1591cfe7dfc849d1944ac53f0d2037dcf17
robertyen2023 <robertyen2023@gmail.com>
2024-03-17 21:13:04 +0800

    9.122.2. Render cartItems (the rough version) in the checkout page.

--- src/routes/Checkout/Checkout.jsx
+++ src/routes/Checkout/Checkout.jsx
@@ -1,8 +1,37 @@
+import { useContext } from 'react';
+
 import './Checkout.scss';
+import { CartContext } from '../../contexts/cart-context';
 
 const Checkout = () => {
+    const {
+        cartItems,
+    } = useContext(CartContext);
+
+    const cartItemList = cartItems.map((cartItem) => {
+        const { 
+            id, 
+            name, 
+            quantity 
+        } = cartItem;
+
+        return (
+            <div key={id}>
+                <h2>{name}</h2>
+                <span>{quantity}</span>
+                <br />
+                <span>decrement</span>
+                <br />
+                <span>increment</span>
+            </div>
+        );
+    });
+
     return (
-        <div className='checkout-container'>Checkout Page</div>
+        <div className='checkout-container'>
+            <h1>I am the checkout page.</h1>
+            <div>{cartItemList}</div>
+        </div>
     );
 };
 
